2b98b4e0b1ac29e5e126fbfd7360ea96ca547bdb
SystemUser32 <51892834+SystemUser32@users.noreply.github.com>
2021-09-10 18:07:57 -0400

    Add files via upload

---
+++ CsharpGenerics_List/CsharpGenerics_List/ListContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsharpGenerics_List
+{
+    public class ListContainer
+    {
+
+        List<string> li = new List<string>();
+
+        public void _Add_(string x)
+        {
+            li.Add(x);
+        }
+
+        public string Value(int x)
+        {
+            return li[x];
+        }
+    }
+}
---
+++ CsharpGenerics_List/CsharpGenerics_List/MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CsharpGenerics_List;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+
+    ListContainer lc = new ListContainer();
+    string[] assets;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        FillArray();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void Display(object sender, EventArgs e)
+    {
+        Random rand = new Random();
+        int num = rand.Next(assets.Length);
+
+        foreach(string i in assets)
+        {
+            lc._Add_(i);
+        }
+
+        label1.Text = lc.Value(num).ToString();
+
+    }
+
+    protected void FillArray()
+    {
+        assets = new string[]
+        {
+            "Mikoyan i Gurevich",
+            "Ilyushin",
+            "Antonov",
+            "Sukhoi",
+            "Yakovlev",
+            "Myasishchev"
+        };
+    }
+}
+
